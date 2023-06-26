ba86e3faf7db3c13be0f041982d6d7417d458f61
Mallikarjun-Mathapati <137722426+Mallikarjun-Mathapati@users.noreply.github.com>
2023-06-26 17:32:34 +0530

    Update App.js
    update path

--- src/App.js
+++ src/App.js
@@ -13,11 +13,11 @@ function App() {
     <>
         <NavBar></NavBar>
       <Routes>
-        <Route exect path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="vehiclemodels" element={<VehicleModels/>} />
-        <Route path="testimonials" element={<Testimonials />} />
-        <Route path="contact" element={<Contact />} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="about" element={<About />} />
+        <Route exact path="vehiclemodels" element={<VehicleModels/>} />
+        <Route exact path="testimonials" element={<Testimonials />} />
+        <Route exact path="contact" element={<Contact />} />
       </Routes>
       <Footer></Footer>
       
